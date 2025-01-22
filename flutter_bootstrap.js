cc21c1f844363d9692f5c959b243d0a3e544a31e
Azurba2 <127968125+Azurba2@users.noreply.github.com>
2025-01-21 19:07:13 -0700

    Add files via upload

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1647082268"
+    serviceWorkerVersion: "2807164039"
   }
 });
